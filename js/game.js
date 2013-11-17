2006633e6de73caeee900e820ee11818728f5a29
Kamil Gałuszka <kgaluszka@nglogic.com>
2013-11-17 11:06:53 +0100

    update

--- js/game.js
+++ js/game.js
@@ -1,9 +1,16 @@
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', { preload: preload, create: create });
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, render: render });
+
+var circle;
 
 function preload(){
 
 }
+
 function create(){
+    circle = new Phaser.Circle(game.world.centerX, game.world.centerY, 64);
 
 }
 
+function render(){
+    game.debug.renderCircle(circle,'#cfffff');
+}
