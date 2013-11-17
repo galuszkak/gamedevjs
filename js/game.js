f4b1afdf528041af3bb9d8eaf712dcdc151b032d
Szymon Nowak <szimek@gmail.com>
2013-11-17 14:56:01 +0100

    Walk left/right

--- js/game.js
+++ js/game.js
@@ -5,68 +5,84 @@ var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', {
     update: update
 });
 
-var planet, player1, player2, cursors, jumpButton;
+var planet, player, cursors, jumpButton, facing = 'left', jumpTimer = 0;
 
 function preload() {
     game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 }
 
 function create() {
-    planet = new Phaser.Circle(game.world.centerX, game.world.centerY, 100);
-    planet.r = planet.diameter/2;
+    player = game.add.sprite(32, 32, 'dude');
+    player.body.bounce.y = 0.2;
+    player.body.collideWorldBounds = true;
+    player.body.gravity.y = 6;
+    player.body.setSize(16, 32, 8, 16);
+    player.anchor.setTo(0.5, 0.5);
 
-    player1 = game.add.sprite(32, 32, 'dude');
-    player1.body.collideWorldBounds = true;
-    player1.body.setSize(16, 32, 8, 16);
-    player1.anchor.setTo(0.5, 0.5);
-    player1.body.allowRotation = true;
+    player.animations.add('left', [0, 1, 2, 3], 10, true);
+    player.animations.add('turn', [4], 20, true);
+    player.animations.add('right', [5, 6, 7, 8], 10, true);
 
-//    player1.animations.add('left', [0, 1, 2, 3], 10, true);
-//    player1.animations.add('turn', [4], 20, true);
-//    player1.animations.add('right', [5, 6, 7, 8], 10, true);
-//
-//    cursors = game.input.keyboard.createCursorKeys();
-//    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-}
-
-function render() {
-    game.debug.renderCircle(planet, "#FAFAFA");
-    game.debug.renderSpriteBounds(player1);
-    game.debug.renderSpriteBody(player1);
+    cursors = game.input.keyboard.createCursorKeys();
+    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 }
 
 function update () {
-    if(planet.distance(player1) >=100){
-        game.physics.accelerateToObject(player1, planet, 60, 200, 200);
-        move_dude()
+    player.body.velocity.x = 0;
+
+    if (cursors.left.isDown)
+    {
+        player.body.velocity.x = -150;
+
+        if (facing != 'left')
+        {
+            player.animations.play('left');
+            facing = 'left';
+        }
     }
-    else{
-        player1.body.velocity.x = 0;
-        player1.body.velocity.y = 0;
-        game.physics.velocityFromAngle(player1.angle, 200, player1.body.velocity);
+    else if (cursors.right.isDown)
+    {
+        player.body.velocity.x = 150;
 
+        if (facing != 'right')
+        {
+            player.animations.play('right');
+            facing = 'right';
+        }
+    }
+    else
+    {
+        if (facing != 'idle')
+        {
+            player.animations.stop();
+
+            if (facing == 'left')
+            {
+                player.frame = 0;
+            }
+            else
+            {
+                player.frame = 5;
+            }
+
+            facing = 'idle';
+        }
     }
 
-    player1.body.angularVelocity = 0;
-
-
+    if (jumpButton.isDown && player.body.touching.down && game.time.now > jumpTimer)
+    {
+        player.body.velocity.y = -250;
+        jumpTimer = game.time.now + 750;
+    }
 }
 
-function move_dude(){
-    if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
-        game.physics.velocityFromAngle(player1.angle, 200, player1.body.velocity);
+function render() {
+    game.debug.renderSpriteInfo(player, 32, 32);
+    var x = Math.round(player.x);
 
-    }
-    if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
-        game.physics.velocityFromAngle(player1.angle-180, 200, player1.body.velocity);
-
-    }
-    if (game.input.keyboard.isDown(Phaser.Keyboard.UP)){
-        game.physics.velocityFromAngle(player1.angle-90, 200, player1.body.velocity);
-
-    }
-    if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
-        game.physics.velocityFromAngle(player1.angle+90, 200, player1.body.velocity);
-
+    if (x <= 0) {
+        player.x = game.world.width - 16 - 1;
+    } else if (x >= game.world.width - 16) {
+        player.x = 1;
     }
 }
