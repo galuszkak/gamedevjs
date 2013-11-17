a9987151384deaa0f8270e883cad9aaaefb91945
Szymon Nowak <szimek@gmail.com>
2013-11-17 11:54:17 +0100

    Add orbiting dude.

--- js/game.js
+++ js/game.js
@@ -1,16 +1,37 @@
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, render: render });
+var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game', {
+    preload: preload,
+    create: create,
+    render: render,
+    update: update
+});
 
-var circle;
+var planet, player1, player2, cursors, jumpButton;
 
-function preload(){
-
+function preload() {
+    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 }
 
-function create(){
-    circle = new Phaser.Circle(game.world.centerX, game.world.centerY, 64);
+function create() {
+    planet = new Phaser.Circle(game.world.centerX, game.world.centerY, 256);
 
+    player1 = game.add.sprite(32, 32, 'dude');
+    player1.body.bounce.y = 0.2;
+    player1.body.collideWorldBounds = true;
+    player1.body.setSize(16, 32, 8, 16);
+
+    player1.animations.add('left', [0, 1, 2, 3], 10, true);
+    player1.animations.add('turn', [4], 20, true);
+    player1.animations.add('right', [5, 6, 7, 8], 10, true);
+
+    cursors = game.input.keyboard.createCursorKeys();
+    jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 }
 
-function render(){
-    game.debug.renderCircle(circle,'#cfffff');
+function render() {
+    game.debug.renderCircle(planet,'#cfffff');
+    game.debug.renderSpriteBody(player1);
+}
+
+function update () {
+    game.physics.accelerateToObject(player1, planet, 60, 200, 200);
 }
